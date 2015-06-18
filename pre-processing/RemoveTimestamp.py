38ebf42167159feaf01870b7bea8f2cdac110812
Nikita Nangia <nikita@mkone.co>
2015-06-18 16:05:40 -0700

    Changing 'Numbers' from list to tuple and adding pyton wrapper to script.

--- pre-processing/RemoveTimestamp.py
+++ pre-processing/RemoveTimestamp.py
@@ -1,11 +1,17 @@
 import sys
 
-######################
 
-Numbers = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
+def main():
 
-script = sys.stdin.readlines()
+	Numbers = ( '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' )
+	script = sys.stdin.readlines()
+	
+	for line in script:
+		if not line.lstrip().startswith( Numbers ):
+			sys.stdout.write( line )
 
-for line in script:
-	if not line.lstrip().startswith( tuple( Numbers ) ):
-		sys.stdout.write( line )
+	pass
+
+
+if __name__ == '__main__':
+	main()
